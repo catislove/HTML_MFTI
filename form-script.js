2b3b71d06879875b59971e1e7ebbb2d4321317bf
Daniil Pilipenko <sortedmap@gmail.com>
2024-10-25 19:24:29 +0300

    initial

---
+++ form-script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let pricePerKm = 20
+    let pricePerKg = 5
+    let maxLength = 2000
+
+    let total = document.querySelector('.total');
+    let totalCost = document.querySelector('.total span');
+    let error = document.querySelector('.error');
+
+    document
+        .querySelector('#calculateButton')
+        .addEventListener('click', () => {
+            let distance = document.querySelector("#distance").value;
+            let weight = document.querySelector("#weight").value;
+            let length = document.querySelector("#length").value;
+
+            let errorText = '';
+            
+            if (length > maxLength) {
+                errorText = 'Длина больше допустимой<br>';
+            }
+
+            if (distance <= 0) {
+                errorText += 'Расстояние не указано<br>';
+            }
+
+            if (weight <= 0) {
+                errorText += 'Масса не указана<br>';
+            }
+
+            if (errorText) {
+                total.style.display = 'none';
+                error.style.display = 'block';
+                error.innerHTML = errorText;
+            } else {
+                error.style.display = 'none';
+                
+                let result = pricePerKm * distance +
+                    pricePerKg * weight;
+                totalCost.innerHTML = result;
+
+                total.style.display = 'block';
+            }
+        });
+});
